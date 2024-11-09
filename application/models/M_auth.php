902a8504f95c8f125b0be848d8ea00e5106d007d
Rizki008 <muhammadrizkinurhamdani@gemail.com>
2024-11-09 23:07:22 +0700

    auth login user

---
+++ application/models/M_auth.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowes');
+
+class M_auth extends CI_Model
+{
+	public function login_user($username, $password)
+	{
+		$this->db->select('*');
+		$this->db->from('user');
+		$this->db->where(array(
+			'username' => $username,
+			'password' => $password
+		));
+		return $this->db->get()->row();
+	}
+}
